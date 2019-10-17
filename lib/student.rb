c2ef27716cc894c2fd06c9846a835561e76c1780
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-17 21:08:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,5 +47,21 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
    end 
   end 
+  
+  def self.create 
+  end 
+  
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+  end 
+  
+  def self.find_by_name
+  end 
+    
+  def update 
+  end 
 
 end
