d417457472aa434795c7f743a33d1667e42d33bf
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-17 21:41:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,10 +55,7 @@ class Student
   end 
   
   def self.new_from_db(row)
-    student = self.new(row)
-    student.id = row[0]
-    student.name = row[1]
-    student.grade = row[2]
+    new_student = self.new(row[0], row[1], row[2])
   end
   
   def self.find_by_name(name)
@@ -66,8 +63,9 @@ class Student
      SELECT * 
      FROM students
      WHERE name = ?
-     LIMIT 1
     SQL
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end 
     
   def update 
